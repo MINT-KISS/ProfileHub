78ab4327419431c00d8e15ceae24fad7338d7274
MINT-KISS <jokerx573@gmail.com>
2024-09-29 20:53:19 +0300

    change ApplicationDbContext construction

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,8 @@ using ProfileHub.Models;
 
 namespace ProfileHub.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
         public DbSet<User> Users { get; set; }
     }
 }

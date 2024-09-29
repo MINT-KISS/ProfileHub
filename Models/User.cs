c5948ff3b79ea22ec6f222bf68a6fba4d19b0ef7
MINT-KISS <jokerx573@gmail.com>
2024-09-29 20:48:13 +0300

    Create DataBase folder, setup database context and initial migration, add User model and UsersController

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ProfileHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Surname { get; set; }
+        public required string Name { get; set; } 
+        public string? Patronymic { get; set; }
+        public string? ProfilePhotoUrl { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProfileHub.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
